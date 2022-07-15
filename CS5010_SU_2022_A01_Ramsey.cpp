8803d8a5540e690eb8a6c9fb122186f0088eec75
tammiramsey <tammirice@gmail.com>
2022-07-14 22:43:34 -0400

    re-do on unsaved files for assignment 1, completing assignment 2

--- CS5010_SU_2022_A01_Ramsey.cpp
+++ CS5010_SU_2022_A01_Ramsey.cpp
@@ -1,5 +1,74 @@
-for (int i=0; i <= (input1Length -1); ++i) {
-		        if ((userInput1.at(i)) != (userInput2.at(i))) {
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    //declaration of variables here
+    string Input1;
+    string Input2;
+    int hammingDistance = 0; // calculated Hamming Distance
+    int input1Length = 0; // length of string 1
+    int input2Length = 0; // length of string 2
+
+    
+    
+    // introduction
+    cout << "This program will calculate the Hamming distance of two strings \n" << endl;
+
+    //user instructions
+    cout << "Please enter two string values when prompted. \n";
+    cout << "Blank string values will not be accepted. \n" << endl;
+
+
+
+    //conversion for -99 Do I need to? no. but leaving this here to remind me I thought about this already
+
+    //first condition: While input is not -99     
+    while ((Input1 != "-99") || (Input2 != "-99")) {
+        hammingDistance = 0; // reset to zero for each loop
+
+        cout << "Enter the first string value" << endl;
+        getline(cin, Input1);
+        
+        // check sentinal value
+        if (Input1 == "-99") {
+            cout << "break-1";
+            break;
+        }
+
+        //testing loop to prevent blank strings (needed only if I switch to getline (I did))
+        while (Input1.empty()) {
+            cout << "No blank strings accepted. \n Enter the first string to compare (or -99 to end the program)" << endl;
+            getline(cin, Input1);
+        }
+        
+        cout << "Enter the second string value \n (make sure they are the same length)" << endl;
+        getline(cin, Input2);
+       
+        // check sentinal value
+        if (Input2 == "-99") {
+            cout << "break-2";
+            break;
+        }
+        
+        //testing loop to prevent blank strings (needed only if I switch to getline (I did))
+        while (Input2.empty()) {
+            cout << "No blank strings accepted \n Enter the second string to compare (or -99 to end the program)" << endl;;
+            getline(cin, Input2);
+        }
+
+        // string length check
+        input1Length = (Input1.size());
+        input2Length = (Input2.size());
+
+        while (input1Length == input2Length) { 
+        /*  
+            cout << "length of 1: " << input1Length << endl; //error checking comment out
+            cout << "length of 2: " << input2Length << endl; //error checking comment out
+        */  
+            // calculate Hamming Distance Here
+            for (int i=0; i <= (input1Length -1); ++i) {
+		        if ((Input1.at(i)) != (Input2.at(i))) {
 			        hammingDistance += 1;
 		        }
 	        }
@@ -9,7 +78,7 @@ for (int i=0; i <= (input1Length -1); ++i) {
         
         }
         if (input1Length != input2Length) {
-            cout << "WARNING: Inputs must be the same length to compare" << endl;
+            cout << "Caution: Input values do not match. Please make sure values match for comparison" << endl;
         }
 
     }
--- CS5010_SU_2022_A02_Ramsey/CS5010_SU_2022_A02_Ramsey.cpp
+++ CS5010_SU_2022_A02_Ramsey/CS5010_SU_2022_A02_Ramsey.cpp
@@ -5,3 +5,83 @@
 
 using namespace std;
 int main() {
+    Clock Clock1("Clock 1", 1, 59, 88);
+	Clock Clock2 ("Clock 2");
+	Clock Clock3("Clock 3", 1, 12);
+	Clock Clock4("Clock 4", 24);
+	Clock Clock5("Clock 5", 5, 45, 45);
+	Clock Clock6("Clock 6", 5, 45, 45);
+
+	cout << "\n Initial Settings Of All Clocks" << endl;
+
+	Clock1.Print();
+	Clock2.Print();
+	Clock3.Print();
+	Clock4.Print();
+	Clock5.Print();
+	Clock6.Print();
+
+	cout << "\n ++++++++++ Adding and Subracting Section ++++++++++" << endl;
+
+	cout << "Adding 300 minutes to Clock 3 expecting X \n", Clock3.addMinutes(3500);
+	cout << "\nAdding 10000 seconds to Clock 2 expecting 2 hours 46 minutes 40 seconds \n", Clock2.addSeconds(50000);
+	cout << "\nAdding 23 hours to Clock 1 expecting 1 day 1 hour 0 minutes 28 seconds \n", Clock1.addHours(13);
+	cout << "\nSubtracting 5 hours from Clock 4 expecting 19 hours 0 minutes 0 seconds \n", Clock4.subtractHours(8);
+	cout << "\nSubtracting 17 minutes from Clock 1 expecting 14 hours 42 minutes 28 seconds \n", Clock1.subtractMinutes(5);
+	cout << "\nSubtracting 27 seconds from Clock 5 expecting 5 hours 45 minutes 18 seconds \n", Clock2.subtractSeconds(77);
+
+	cout << "\n\n Time Format Printing Test" << endl;
+	cout << "\nPrinting Time expecting 24 hour format \n" , Clock1.PrintTime(false);
+	cout << "\nPrinting Time expecting AM/PM format \n", Clock1.PrintTime(true);
+	cout << "\nPrinting Time expecting 24hr format \n" , Clock2.PrintTime(false);
+	cout << "\nPrinting Time expecting AM/PM format  \n", Clock2.PrintTime(true); 
+	cout << "\nPrinting Time expecting 24hr format  \n" , Clock3.PrintTime(false);
+	cout << "\nPrinting Time expecting AM/PM format \n", Clock3.PrintTime(true);
+	cout << "\nPrinting Time expecting 24hr format \n" , Clock4.PrintTime(false);
+	cout << "\nPrinting Time expecting AM/PM format \n", Clock4.PrintTime(true);
+	
+	cout << "\n\n Comparisons between clocks" << endl;
+	
+	Clock5.Print(), Clock6.Print();
+	if (Clock5 == Clock6) {
+		cout << "These Clocks are the SAME \n" << endl;
+	}
+	else {
+		cout << "ALERT" << endl;
+	}
+	Clock2.Print(), Clock3.Print();
+	if (Clock2 != Clock3) {
+		cout << "These clocks are NOT the same \n" << endl;
+	}
+	else {
+		cout << "ALERT" << endl;
+	}
+	Clock2.Print(), Clock1.Print();
+	if (Clock2 < Clock1) {
+		cout << "Clock 2 is less than Clock 1 \n" << endl;
+	}
+	else {
+		cout << "ALERT" << endl;
+	}	
+	if (Clock5 < Clock6) {
+		cout << "This error message means something is incorrect";
+	}
+	else {
+		cout << "If you see this message, everything is running as anticiapted\n" << endl;
+	}
+	Clock6.addSeconds(1), Clock5.Print();
+	if (Clock6 >= Clock5) {
+		cout << "Clock6 is greater than or equal to Clock 5\n" << endl;
+	}
+	else {
+		cout << "ALERT" << endl;
+	}
+
+
+
+
+
+
+  return 0;
+
+}
